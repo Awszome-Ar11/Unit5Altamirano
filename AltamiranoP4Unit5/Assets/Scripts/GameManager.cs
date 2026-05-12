7f57fe2c750ad97a0d041f9f60cc1cfe3592570e
Awszome-Ar11 <743241@student.dusd.net>
2026-05-12 10:50:46 -0700

    [op[
    ]op[

--- AltamiranoP4Unit5/Assets/Scripts/GameManager.cs
+++ AltamiranoP4Unit5/Assets/Scripts/GameManager.cs
@@ -1,16 +1,21 @@
 using UnityEngine;
 using System.Collections.Generic;
 using System.Collections;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     private float SpawnRate = 1.0f;
     public List<GameObject> targets;
+    private int score;
+    public TextMeshProUGUI scoreText;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         StartCoroutine(SpawnTarget());
+        score = 0;
+        UpdateScore(0);
     }
 
     // Update is called once per frame
@@ -25,6 +30,14 @@ public class GameManager : MonoBehaviour
             yield return new WaitForSeconds(SpawnRate);
             int index = Random.Range(0, targets.Count);
             Instantiate(targets[index]);
+
+            UpdateScore(5);
         }
+    }
+
+    void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoreText.text = "Score: " + score;
     }
 }
